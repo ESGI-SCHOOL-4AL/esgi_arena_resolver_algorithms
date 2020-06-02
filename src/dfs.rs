5466e694d07831ce9606da082165d3a30891e28b
LAURENT Louis <louis_laurent@aol.fr>
2020-06-02 19:57:11 +0200

    add exploration function for all path between 2 points
    need to change the display to a returned struct
    need to ignore walls

--- src/dfs.rs
+++ src/dfs.rs
@@ -31,23 +31,40 @@ fn dfs_fs_aps(fs: Vec<Field>, aps: Vec<u8>, start: Field, matrix_size: usize) ->
 
 }
 
-fn dfs_fs_aps_recursive(fs: Vec<Field>, aps: Vec<u8>, start: Field, matrix_size: usize, discovered: &mut Vec<Field>) {
+fn dfs_fs_aps_recursive(fs: Vec<Field>, aps: Vec<u8>, start_end: (Field, Field), matrix_size: usize, discovered: &mut Vec<Field>, current_path: &mut Vec<Field>) {
+    let (start, end) = start_end;
     discovered.push(start);
-    println!("{:?}", start);
+
+    let current_field_discover_index = discovered.len() - 1;
+
+    if start == end {
+        for element in current_path.clone() {
+            println!("len, {}, element path: {:?}", current_path.len(), element);
+        }
+        println!();
+        return;
+    }
 
     for child in get_element_childs_from_fs_aps(fs.clone(), aps.clone(), start.coordinates.get_index(matrix_size).unwrap()).unwrap() {
         let mut is_discover = false;
+        
         for discovered_field in discovered.to_owned() {
             if discovered_field == child {
                 is_discover = true;
             }
         }
+
         if is_discover {
             continue;
         }
 
-        dfs_fs_aps_recursive(fs.clone(), aps.clone(), child, matrix_size, discovered);
+        current_path.push(child);
+        let current_field_index_path = current_path.len() - 1;
+        dfs_fs_aps_recursive(fs.clone(), aps.clone(), (child, end), matrix_size, &mut discovered.clone(), &mut current_path.clone());
+        current_path.remove(current_field_index_path);
     }
+
+    discovered.remove(current_field_discover_index);
 
 }
 
@@ -200,8 +217,15 @@ mod test {
             },
             value: Some(1)
         };
+        let end_field_example = Field {
+            coordinates: Point {
+                x: Some(1),
+                y: Some(1)
+            },
+            value: Some(2)
+        };
 
-        dfs_fs_aps_recursive(fs_example, aps_example, start_field_example, 2, &mut Vec::new());
+        dfs_fs_aps_recursive(fs_example, aps_example, (start_field_example, end_field_example), 2, &mut Vec::new(), &mut vec![start_field_example]);
 
         assert_eq!(2, 4);
         
